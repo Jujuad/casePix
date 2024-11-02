9d5d97f03119553528bcc1ffc896f0a8e9add4e4
julia.andrade <julia.andrade@dpsp.com.br>
2024-11-02 16:35:51 -0300

    Addition dtos - of creation

---
+++ src/main/java/com/example/demo/dto/ChavePixCreateDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+public class ChavePixCreateDTO {
+
+    @NotNull(message = "Conta não pode ser nula.")
+    private UUID contaId;
+
+    @NotNull(message = "Tipo de chave não pode ser nulo.")
+    @Size(max = 9, message = "Tipo de chave deve ter no máximo 9 caracteres.")
+    private String tipoChave;
+
+    @NotNull(message = "Valor da chave não pode ser nulo.")
+    @Size(max = 77, message = "Valor da chave deve ter no máximo 77 caracteres.")
+    private String valorChave;
+}
