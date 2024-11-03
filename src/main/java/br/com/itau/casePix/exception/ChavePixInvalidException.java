1230e0114d21f6402ddaffadda39368166bfaeaf
julia.andrade <julia.andrade@dpsp.com.br>
2024-11-03 10:39:10 -0300

    Addition of exceptions

---
+++ src/main/java/br/com/itau/casePix/exception/ChavePixInvalidException.java
@@ -0,0 +1,7 @@
+package br.com.itau.casePix.exception;
+
+public class ChavePixInvalidException extends RuntimeException {
+    public ChavePixInvalidException(String message) {
+        super(message);
+    }
+}
